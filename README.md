c1e4248c2b816e67e4040b2f2c9063945dfe27f7
slavlee <info@slavlee.de>
2024-07-18 10:09:25 +0200

    ## Version 0.0.1 - [IMPORTANT] Create github repository - [IMPORTANT] Kickstart Extension

---
+++ README.md
@@ -0,0 +1,22 @@
+# TYPO3 Extension: easyshop
+TYPO3 Extension to create a simple shop with paypal express checkout only.
+
+## 2 Usage
+
+### 2.1 Installation
+
+#### Installation as extension from TYPO3 Extension Repository (TER)
+Download and install the [extension][1] with the extension manager module.
+
+#### 2.2 Installation with composer
+`composer req wacon/easyshop`
+
+## 3 Todos
+- DB model
+- Template
+- PayPal Interface
+
+## 4 Changelog
+see [CHANGES.md](CHANGES.md)
+
+[1]: https://extensions.typo3.org/extension/easyshop
---
+++ ext_emconf.php
@@ -0,0 +1,26 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Easyshop',
+    'description' => 'TYPO3 Extension to create a simple shop with paypal express checkout only.',
+    'category' => 'plugin',
+    'author' => 'Kevin Chileong Lee',
+    'author_email' => 'info@wacon.de',
+    'author_company' => 'WACON Internet GmbH',
+    'state' => 'stable',
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-12.4.99'
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Wacon\\Easyshop\\' => 'Classes',
+        ],
+    ],
+];
